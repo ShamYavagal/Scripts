f905c4fe2b1cd9e1532d49f15ced2be9c39e528b
Sham Yavagal <shamyavagal@gmail.com>
2021-05-17 20:16:18 -0400

    changes

---
+++ DeleteBucketObject.java
@@ -0,0 +1,41 @@
+package s3;
+
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+
+class DeleteBucketObject {
+	
+	public static void main(String[] args) {
+		final BasicAWSCredentials awsCreds = new BasicAWSCredentials(Credentials.access_key_id, Credentials.secret_access_key);
+		
+		
+		/* @SuppressWarnings("deprecation")
+		AmazonS3 s3 = new AmazonS3Client(awsCreds); */
+		
+	     /* Region usEast1 = Region.getRegion(Regions.US_EAST_1);
+			s3.setRegion(usEast1); */
+		
+		AmazonS3 s3 = AmazonS3ClientBuilder.standard()
+                .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
+                .withRegion(Regions.US_EAST_1)
+                .build();
+						
+		String bucket_name = "sniorigin-test";
+		String object_key = "test1.txt";
+		
+		try {
+			s3.deleteObject(bucket_name, object_key);
+		} catch (AmazonServiceException e) {
+			System.err.println(e.getErrorMessage());
+			System.exit(1);
+		}
+		
+    }
+
+}
